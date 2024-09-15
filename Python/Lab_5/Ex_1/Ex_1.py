fe77635470cb4859a5d17681d42774bda6eed86b
What-1s-love <belikov.vlad.2016@gmail.com>
2024-09-15 12:20:57 +0200

    Lab5 Ex1

---
+++ Python/Lab_5/Ex_1/Ex_1.py
@@ -0,0 +1,35 @@
+﻿def generate_fibonacci(n):
+    # Створення пустого списку для зберігання чисел Фібоначчі
+    fibonacci_sequence = []
+
+    # Перевірка, чи n дійсне число
+    if n <= 0:
+        print("Кількість елементів повинна бути додатним числом.")
+        return fibonacci_sequence
+
+    # Додавання перших елементів послідовності Фібоначчі
+    a, b = 1, 1
+    for _ in range(n):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    return fibonacci_sequence
+
+def main():
+    try:
+        # Запит користувача про довжину масиву
+        n = int(input("Введіть кількість елементів послідовності Фібоначчі: "))
+        
+        # Генерація послідовності Фібоначчі
+        fibonacci_sequence = generate_fibonacci(n)
+        
+        # Виведення результату
+        print("Послідовність Фібоначчі:")
+        print(fibonacci_sequence)
+    
+    except ValueError:
+        print("Будь ласка, введіть дійсне число.")
+
+if __name__ == "__main__":
+    main()
+
