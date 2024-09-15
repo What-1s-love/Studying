5d15d9093a930c5cbb914ac4cf227892ed8fbbdc
What-1s-love <belikov.vlad.2016@gmail.com>
2024-09-15 12:35:37 +0200

    Lab5 Ex2

---
+++ Python/Lab_5/Ex_2/Ex_2.py
@@ -0,0 +1,12 @@
+﻿# Створюємо порожній масив 7x7
+matrix = [[0 for _ in range(7)] for _ in range(7)]
+
+# Заповнюємо масив за зразком
+for i in range(7):
+    for j in range(7 - i):
+        matrix[i][j] = i + j + 1
+
+# Виводимо масив на екран
+for row in matrix:
+    print(row)
+
