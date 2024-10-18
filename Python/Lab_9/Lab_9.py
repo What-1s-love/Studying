181a268aa2d113df4a188e7c6ef3402b31371cf8
What-1s-love <belikov.vlad.2016@gmail.com>
2024-10-18 21:17:44 +0200

    Lab_9_new

--- Python/Lab_9/Lab_9.py
+++ Python/Lab_9/Lab_9.py
@@ -1,21 +1,58 @@
 ﻿import string
 
-# a) Створюємо файл TF1_1.txt і записуємо в нього текст
-with open('TF1_1.txt', 'w', encoding='utf-8') as file1:
-    file1.write("Привіт, це перший тестовий рядок!\n")
-    file1.write("Ось ще один рядок з різними, розділовими знаками.\n")
-    file1.write("Python - це чудова мова програмування.")
+# Функція для відкриття файлу з обробкою помилок
+def open_file(file_name, mode):
+    try:
+        file = open(file_name, mode, encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Файл {file_name} не був знайдений!")
+        return None
+    except IOError:
+        print(f"Помилка вводу/виводу при відкритті файлу {file_name}!")
+        return None
+    else:
+        print(f"Файл {file_name} був успішно відкритий!")
+        return file
 
-# б) Читаємо вміст файла TF1_1, видаляємо розділові знаки і записуємо кожне слово в новий файл TF1_2.txt
-with open('TF1_1.txt', 'r', encoding='utf-8') as file1, open('TF1_2.txt', 'w', encoding='utf-8') as file2:
-    for line in file1:
-        # Видаляємо всі розділові знаки за допомогою translate і string.punctuation
-        words = line.translate(str.maketrans('', '', string.punctuation)).split()
-        # Записуємо кожне слово в новий рядок у файл TF1_2.txt
-        for word in words:
-            file2.write(word + '\n')
+# а) Створюємо файл TF1_1.txt і записуємо в нього текст
+file1_name = 'TF1_1.txt'
+file1 = open_file(file1_name, 'w')
+if file1:
+    try:
+        file1.write("Привіт, це перший тестовий рядок!\n")
+        file1.write("Ось ще один рядок з різними, розділовими знаками.\n")
+        file1.write("Python - це чудова мова програмування.")
+        print(f"Інформація успішно записана в {file1_name}!")
+    finally:
+        file1.close()
+        print(f"Файл {file1_name} був закритий!")
 
-# в) Читаємо вміст файла TF1_2.txt і виводимо його на екран
-with open('TF1_2.txt', 'r', encoding='utf-8') as file2:
-    for line in file2:
-        print(line.strip())
+# б) Читаємо вміст файлу TF1_1.txt, видаляємо розділові знаки і записуємо кожне слово в новий файл TF1_2.txt
+file1 = open_file(file1_name, 'r')
+file2_name = 'TF1_2.txt'
+file2 = open_file(file2_name, 'w')
+
+if file1 and file2:
+    try:
+        for line in file1:
+            # Видаляємо всі розділові знаки
+            words = line.translate(str.maketrans('', '', string.punctuation)).split()
+            # Записуємо кожне слово в новий рядок у файл TF1_2.txt
+            for word in words:
+                file2.write(word + '\n')
+        print(f"Слова успішно записані в {file2_name}!")
+    finally:
+        file1.close()
+        file2.close()
+        print(f"Файли {file1_name} та {file2_name} були закриті!")
+
+# в) Читаємо вміст файлу TF1_2.txt і виводимо його на екран
+file2 = open_file(file2_name, 'r')
+
+if file2:
+    try:
+        for line in file2:
+            print(line.strip())
+    finally:
+        file2.close()
+        print(f"Файл {file2_name} був закритий!")
