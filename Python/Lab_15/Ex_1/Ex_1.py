8c7d159b6f92d07ec7cc0705b1c2218e432a1fb2
What-1s-love <belikov.vlad.2016@gmail.com>
2024-11-22 20:56:10 +0100

    Lab_15

---
+++ Python/Lab_15/Ex_1/Ex_1.py
@@ -0,0 +1,28 @@
+﻿import pandas as pd
+
+# Створення DataFrame з існуючого словника
+data = {
+    "Vitaly_Prikhodko": [12, 10, 9, 10, 5, 7, 8, 5, 12, 10],
+    "Dmytro_Kropyvnytskyi": [12, 10, 9, 5, 6, 7, 4, 3, 12, 4],
+    "Mikhail_Romanenko": [12, 3, 4, 6, 5, 5, 3, 7, 5, 4],
+    "Maxim_Derizemlya": [12, 4, 10, 7, 5, 8, 3, 3, 5, 7],
+    "Victoria_Zhuk": [10, 10, 10, 9, 10, 9, 10, 8, 7, 12],
+    "Andrey_Kuryanov": [5, 6, 7, 5, 4, 7, 5, 4, 4, 8],
+    "Oksana_Dubovets": [7, 8, 5, 8, 8, 9, 8, 7, 7, 10],
+    "Nikita_Stroganov": [6, 7, 8, 9, 10, 10, 10, 10, 10, 9],
+    "Karina_Nikolaenko": [2, 3, 5, 4, 5, 4, 3, 3, 5, 8]
+}
+
+df = pd.DataFrame(data)
+
+# Виведення DataFrame на екран
+print(df)
+
+# Вибірка для одного учня
+print(df["Vitaly_Prikhodko"])
+
+# Виконання агрегаційних операцій
+print("Середнє значення оцінок Vitaly_Prikhodko = ", df["Vitaly_Prikhodko"].mean())
+print("Сума оцінок Vitaly_Prikhodko = ", df["Vitaly_Prikhodko"].sum())
+print("Мінімальна оцінка Vitaly_Prikhodko = ", df["Vitaly_Prikhodko"].min())
+print("Максимальна оцінка Vitaly_Prikhodko = ", df["Vitaly_Prikhodko"].max())
---
+++ Python/Lab_15/Ex_2/Ex_2.py
@@ -0,0 +1,24 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+# Завантажуємо дані з файлу
+file_path = 'velo_data_2009.csv'
+df = pd.read_csv(file_path, sep=';', encoding='latin1', parse_dates=['Date'], dayfirst=False, index_col='Date')
+
+# Ресемплінг даних по місяцях
+monthly_data = df.resample('ME').sum()
+
+# Знаходимо найбільш популярний місяць
+most_popular_month = monthly_data.idxmax()
+
+print("Найбільш популярний місяць:", most_popular_month, monthly_data.loc[most_popular_month])
+
+# Побудова графіку
+plt.figure(figsize=(10, 6))
+monthly_data.plot(kind='bar', color='skyblue')
+plt.title("Відвідування велодоріжок у 2009 році")
+plt.xlabel("Місяць")
+plt.ylabel("Кількість велосипедистів")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
